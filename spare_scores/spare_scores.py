81bb1a0cc4f9f775ec4f48ae90df96ed3bf97a14
Gyujoon <gyujoon.hwang@pennmedicine.upenn.edu>
2023-01-20 13:01:06 -0500

    Meta data as a class

--- spare_scores/spare_scores.py
+++ spare_scores/spare_scores.py
@@ -1,24 +1,30 @@
-import os
 import gzip
 import pickle
 import logging
 import numpy as np
 import pandas as pd
+from typing import Tuple
+from dataclasses import dataclass
 from spare_scores.svm import run_SVC, run_SVR
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-def load_model(mdl_path: str):
-  with gzip.open(mdl_path, 'rb') as f:
-    return pickle.load(f)
-
+@dataclass
+class MetaData:
+  spare_type: str
+  kernel: str
+  n: int
+  age_range: list
+  to_predict: str
+  predictors: list
+  
 def spare_train(df: pd.DataFrame,
                 predictors: list,
                 to_predict: str,
                 pos_group: str = '',
                 kernel: str = 'linear',
                 save_path: str = None,
-                mdl_name: str = ''):
+                mdl_name: str = '') -> Tuple[dict, dict]:
 
   def _expspace(span: list):
     return np.exp(np.linspace(span[0], span[1], num=int(span[1])-int(span[0])+1))
@@ -36,7 +42,7 @@ def spare_train(df: pd.DataFrame,
     if np.min(df[to_predict].value_counts()) < 10:
       return logging.error('At least one of the groups to classify is too small (n<10).')
     elif np.min(df[to_predict].value_counts()) < 100:
-      logging.warn('At least one of the groups to classify may be too small to build a robust SPARE classification model (n<100).')
+      logging.warn('At least one of the groups to classify may be too small (n<100).')
     if np.sum((df['PTID']+df[to_predict]).duplicated()) > 0:
       logging.warn('Training dataset has duplicate participants.')
     spare_type = 'classification'
@@ -47,7 +53,7 @@ def spare_train(df: pd.DataFrame,
     if len(df.index) < 10:
       return logging.error('Sample size is too small (n<10).')
     elif len(df.index) < 100:
-      logging.warn('Sample size may be too small to build a robust SPARE regression model (n<100).')
+      logging.warn('Sample size may be too small (n<100).')
     if np.sum(df['PTID'].duplicated()) > 0:
       logging.warn('Training dataset has duplicate participants.')
     if pos_group != '':
@@ -60,31 +66,26 @@ def spare_train(df: pd.DataFrame,
     logging.info('Variable to predict is in the predictor set. This will be removed from the set.')
     predictors.remove(to_predict)
   if np.sum(np.sum(pd.isna(df[predictors]))) > 0:
-    logging.info('Some participants have invalid predictor variables (such as n/a). They will be excluded from the training set.')
+    logging.info('Some participants have invalid predictor variables (such as n/a). They will be excluded from the training.')
     df = df.loc[np.sum(pd.isna(df[predictors]), axis=1) == 0].reset_index(drop=True)
   #########################################
 
   # Initiate SPARE model
-  metaData = {'spare_type': spare_type,
-              'kernel':kernel,
-              'n': len(df.index),
-              'age_range': np.floor([np.min(df['Age']), np.max(df['Age'])]),
-              'to_predict': to_predict,
-              'predictors': predictors}
+  meta_data = MetaData(spare_type, kernel, len(df.index), [np.min(df['Age']), np.max(df['Age'])], to_predict, predictors)
 
   # Convert categorical variables
   var_categorical = df[predictors].dtypes == np.object
   var_categorical = var_categorical[var_categorical].index
-  metaData['categorical_var_map'] = dict(zip(var_categorical, [None]*len(var_categorical)))
+  meta_data.categorical_var_map = dict(zip(var_categorical, [None]*len(var_categorical)))
   for var in var_categorical:
     if len(df[var].unique()) == 2:
-      metaData['categorical_var_map'][var] = {df[var].unique()[0]: 1, df[var].unique()[1]: 2}
-      df[var] = df[var].map(metaData['categorical_var_map'][var])
+      meta_data.categorical_var_map[var] = {df[var].unique()[0]: 1, df[var].unique()[1]: 2}
+      df[var] = df[var].map(meta_data.categorical_var_map[var])
 
   # SPARE classification
   if spare_type == 'classification':
     if kernel == 'linear':
-      metaData['pos_group'] = pos_group
+      meta_data.pos_group = pos_group
       param_grid = {'C': _expspace([-9, 5])}
     elif kernel == 'rbf':
       param_grid = {'C': _expspace([-9, 5]), 'gamma': _expspace([-5, 5])}
@@ -93,7 +94,7 @@ def spare_train(df: pd.DataFrame,
                 to_predict, groups_to_classify, param_grid=param_grid, kernel=kernel, n_repeats=1, verbose=0)
       for par in param_grid.keys():
         param_grid[par] = _expspace([np.min(params[f'{par}_optimal']), np.max(params[f'{par}_optimal'])])
-    df['predicted'], mdl, metaData['auc'], metaData['params'] = run_SVC(
+    df['predicted'], mdl, meta_data.auc, meta_data.params = run_SVC(
                 df, predictors, to_predict, groups_to_classify, param_grid=param_grid, kernel=kernel)
 
   # SPARE regression
@@ -104,9 +105,9 @@ def spare_train(df: pd.DataFrame,
                 to_predict, param_grid=param_grid, n_repeats=1, verbose=0)
       for par in param_grid.keys():
         param_grid[par] = _expspace([np.min(params[f'{par}_optimal']), np.max(params[f'{par}_optimal'])])
-    df['predicted'], mdl, metaData['mae'], metaData['params'] = run_SVR(
+    df['predicted'], mdl, meta_data.mae, meta_data.params = run_SVR(
                   df, predictors, to_predict, param_grid=param_grid)
-  metaData['cv_results'] = df[list(dict.fromkeys(['PTID', 'Age', 'Sex', to_predict, 'predicted']))]
+  meta_data.cv_results = df[list(dict.fromkeys(['PTID', 'Age', 'Sex', to_predict, 'predicted']))]
 
   # Save model
   if save_path is not None:
@@ -114,24 +115,28 @@ def spare_train(df: pd.DataFrame,
       to_predict_ = to_predict.replace('.', '_')
       mdl_name = f'SPARE_{spare_type}_{to_predict_}'
     with gzip.open(f'{save_path}/mdl_{mdl_name}.pkl.gz', 'wb') as f:
-      pickle.dump((mdl, metaData), f)
+      pickle.dump((mdl, vars(meta_data)), f)
+      logging.info(f'Model saved to {save_path}/mdl_{mdl_name}.pkl.gz')
 
-  return mdl, metaData
+  return mdl, vars(meta_data)
 
+def load_model(mdl_path: str):
+  with gzip.open(mdl_path, 'rb') as f:
+    return pickle.load(f)
 
 def spare_test(df: pd.DataFrame,
                mdl_path: str) -> pd.DataFrame:
 
   # Load trained SPARE model
-  mdl, metaData = load_model(mdl_path)
+  mdl, meta_data = load_model(mdl_path)
   df = df.copy()
 
   ################ FILTERS ################
-  if not set(metaData['predictors']).issubset(df.columns):
-    cols_not_found = sorted(set(metaData['predictors']) - set(df.columns))
-    assert len([a for a in cols_not_found if '_' not in a]) > 0, f'Not all predictors exist in the input dataframe: {cols_not_found}'
+  if not set(meta_data['predictors']).issubset(df.columns):
+    cols_not_found = sorted(set(meta_data['predictors']) - set(df.columns))
+    assert len([a for a in cols_not_found if '_' not in a]) == 0, f'Not all predictors exist in the input dataframe: {cols_not_found}'
     try:
-      roi_name = [a for a in metaData['predictors'] if '_' in a]
+      roi_name = [a for a in meta_data['predictors'] if '_' in a]
       for roi_alter in [[int(a.split('_')[-1]) for a in roi_name],
                         [a.split('_')[-1] for a in roi_name],
                         ['R'+a.split('_')[-1] for a in roi_name]]:  
@@ -141,48 +146,48 @@ def spare_test(df: pd.DataFrame,
           continue
     except Exception:
       return logging.error(f'Not all predictors exist in the input dataframe: {cols_not_found}')
-    cols_not_found = sorted(set(metaData['predictors']) - set(df.columns))
-    assert len(cols_not_found) > 0, f'Not all predictors exist in the input dataframe: {cols_not_found}'
-  if (np.min(df['Age']) < metaData['age_range'][0]) or (np.max(df['Age']) > metaData['age_range'][1]):
+    cols_not_found = sorted(set(meta_data['predictors']) - set(df.columns))
+    assert len(cols_not_found) == 0, f'Not all predictors exist in the input dataframe: {cols_not_found}'
+  if (np.min(df['Age']) < meta_data['age_range'][0]) or (np.max(df['Age']) > meta_data['age_range'][1]):
     logging.warn('Some participants fall outside the age range of the SPARE model.')
 
-  if np.sum(np.sum(pd.isna(df[metaData['predictors']]))) > 0:
+  if np.sum(np.sum(pd.isna(df[meta_data['predictors']]))) > 0:
     logging.warn('Some participants have invalid predictor variables.')
 
-  if np.any(df['PTID'].isin(metaData['cv_results']['PTID'])):
+  if np.any(df['PTID'].isin(meta_data['cv_results']['PTID'])):
     logging.info('Some participants seem to have been in the model training.')
   #########################################
 
   # Convert categorical variables
-  for var in metaData.get('categorical_var_map',{}).keys():
-    if not isinstance(metaData['categorical_var_map'][var], dict):
+  for var in meta_data.get('categorical_var_map',{}).keys():
+    if not isinstance(meta_data['categorical_var_map'][var], dict):
       continue
-    if np.all(df[var].isin(metaData['categorical_var_map'][var].keys())):
-      df[var] = df[var].map(metaData['categorical_var_map'][var])
+    if np.all(df[var].isin(meta_data['categorical_var_map'][var].keys())):
+      df[var] = df[var].map(meta_data['categorical_var_map'][var])
     else:
-      expected_var = list(metaData['categorical_var_map'][var].keys())
+      expected_var = list(meta_data['categorical_var_map'][var].keys())
       return logging.error(f'Column "{var}" contains value(s) other than expected: {expected_var}')
 
   # Output model description
-  print('Model Info: training N =', metaData['n'], end=' / ')
-  print('ages =', int(metaData['age_range'][0]), '-', int(metaData['age_range'][1]), end=' / ')
-  if metaData['spare_type'] == 'classification':
-    print('expected AUC =', np.round(np.mean(metaData['auc']), 3))
-  elif metaData['spare_type'] == 'regression':
-    print('expected MAE =', np.round(np.mean(metaData['mae']), 3))
+  print('Model Info: training N =', meta_data['n'], end=' / ')
+  print('ages =', int(meta_data['age_range'][0]), '-', int(meta_data['age_range'][1]), end=' / ')
+  if meta_data['spare_type'] == 'classification':
+    print('expected AUC =', np.round(np.mean(meta_data['auc']), 3))
+  elif meta_data['spare_type'] == 'regression':
+    print('expected MAE =', np.round(np.mean(meta_data['mae']), 3))
 
   # Calculate SPARE scores
   n_ensemble = len(mdl['scaler'])
   ss, ss_mean = np.zeros([len(df.index), n_ensemble]), np.zeros([len(df.index), ])
   for i in range(n_ensemble):
-    X = mdl['scaler'][i].transform(df[metaData['predictors']])
-    if metaData['kernel'] == 'linear':
+    X = mdl['scaler'][i].transform(df[meta_data['predictors']])
+    if meta_data['kernel'] == 'linear':
       ss[:, i] = np.sum(X * mdl['mdl'][i].coef_, axis=1) + mdl['mdl'][i].intercept_
     else:
       ss[:, i] = mdl['mdl'][i].decision_function(X)
-    if metaData['spare_type'] == 'regression':
+    if meta_data['spare_type'] == 'regression':
       ss[:, i] = (ss[:, i] - mdl['bias_correct']['int'][i]) / mdl['bias_correct']['slope'][i]
-    ss[df['PTID'].isin(metaData['cv_results']['PTID'][mdl['cv_folds'][i][0]]), i] = np.nan
+    ss[df['PTID'].isin(meta_data['cv_results']['PTID'][mdl['cv_folds'][i][0]]), i] = np.nan
   index_nan = np.all(np.isnan(ss),axis=1)
   ss_mean[~index_nan] = np.nanmean(ss[~index_nan,:], axis=1)
 
