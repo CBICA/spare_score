d21fae88024e6d0c47fc14b04dfbc0136b186b7d
Gyujoon <gyujoon.hwang@pennmedicine.upenn.edu>
2023-04-03 16:32:19 -0400

    Standardize model structures and some refactoring

--- spare_scores/spare_scores.py
+++ spare_scores/spare_scores.py
@@ -8,13 +8,12 @@ from dataclasses import dataclass
 from spare_scores.svm import run_SVC, run_SVR
 from spare_scores.data_prep import check_train, check_test, load_model
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
 @dataclass
 class MetaData:
   """Stores training information on its paired SPARE model
   """
-  spare_type: str
+  mdl_type: str
   kernel: str
   predictors: list
   to_predict: str
@@ -24,6 +23,7 @@ def spare_train(df: Union[pd.DataFrame, str],
                 to_predict: str,
                 pos_group: str = '',
                 kernel: str = 'linear',
+                verbose: int = 1,
                 save_path: str = None) -> Tuple[dict, dict]:
   """Trains a SPARE model, either classification or regression
 
@@ -41,59 +41,47 @@ def spare_train(df: Union[pd.DataFrame, str],
     a tuple of two dictionaries: first to contain SPARE model coefficients, and
       second to contain model information
   """
-
+  if verbose >= 1:
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s', force=True)
   df = _load_df(df)
-  df, predictors, spare_type = check_train(df, predictors, to_predict, pos_group)
-  if spare_type == 'classification':
-    groups_to_classify = [a for a in df[to_predict].unique() if a != pos_group] + [pos_group]
-
-  # Initiate SPARE model
-  meta_data = MetaData(spare_type, kernel, predictors, to_predict)
-  meta_data.n = len(df.index)
-  meta_data.age_range = [np.min(df['Age']), np.max(df['Age'])]
+  df, predictors, mdl_type = check_train(df, predictors, to_predict, pos_group)
+  meta_data = MetaData(mdl_type, kernel, predictors, to_predict)
 
   # Convert categorical variables
-  var_categorical = df[predictors].dtypes == np.object
-  var_categorical = var_categorical[var_categorical].index
-  meta_data.categorical_var_map = dict(zip(var_categorical, [None]*len(var_categorical)))
+  var_categorical = [var for var in df[predictors].columns if df[var].dtype == 'O']
+  meta_data.categorical_var_map = {var: None for var in var_categorical}
   for var in var_categorical:
     if len(df[var].unique()) == 2:
       meta_data.categorical_var_map[var] = {df[var].unique()[0]: 1, df[var].unique()[1]: 2}
       df[var] = df[var].map(meta_data.categorical_var_map[var])
     elif len(df[var].unique()) > 2:
-      return logging.error('Categorical variables with more than 2 categories are currently not supported.')
-
-  # SPARE classification
-  if spare_type == 'classification':
-    if kernel == 'linear':
-      meta_data.pos_group = pos_group
-      param_grid = {'C': _expspace([-9, 5])}
-    elif kernel == 'rbf':
-      param_grid = {'C': _expspace([-9, 5]), 'gamma': _expspace([-5, 5])}
-    if len(df.index) > 1000:
-      _, _, _, params = run_SVC(df.sample(n=500, random_state=2022).reset_index(drop=True), predictors,
-                to_predict, groups_to_classify, param_grid=param_grid, kernel=kernel, n_repeats=1, verbose=0)
-      for par in param_grid.keys():
-        param_grid[par] = _expspace([np.min(params[f'{par}_optimal']), np.max(params[f'{par}_optimal'])])
-    df['predicted'], mdl, meta_data.auc, meta_data.params = run_SVC(
-                df, predictors, to_predict, groups_to_classify, param_grid=param_grid, kernel=kernel)
-
-  # SPARE regression
-  elif spare_type == 'regression':
+      raise ValueError('Categorical variables with more than 2 categories are currently not supported.')
+    
+  # Prepare parameters
+  if mdl_type == 'SVM Classification':
+    meta_data.pos_group = pos_group
+    to_predict_input = [to_predict, [a for a in df[to_predict].unique() if a != pos_group] + [pos_group]]
+    param_grid = {'linear':{'C': _expspace([-9, 5])},
+                  'rbf':{'C': _expspace([-9, 5]), 'gamma': _expspace([-5, 5])}}[kernel]
+  elif mdl_type == 'SVM Regression':
+    to_predict_input = to_predict
     param_grid = {'C': _expspace([-5, 5]), 'epsilon': _expspace([-5, 5])}
-    if len(df.index) > 1000:
-      _, _, _, params = run_SVR(df.sample(n=500, random_state=2022).reset_index(drop=True), predictors,
-                to_predict, param_grid=param_grid, n_repeats=1, verbose=0)
-      for par in param_grid.keys():
-        param_grid[par] = _expspace([np.min(params[f'{par}_optimal']), np.max(params[f'{par}_optimal'])])
-    df['predicted'], mdl, meta_data.mae, meta_data.params = run_SVR(
-                  df, predictors, to_predict, param_grid=param_grid)
-  meta_data.cv_results = df[list(dict.fromkeys(['ID', 'Age', 'Sex', to_predict, 'predicted']))]
 
+  # Train model
+  svms = {'SVM Classification': run_SVC, 'SVM Regression': run_SVR}
+  if len(df.index) > 1000:
+    logging.info('Due to large dataset, first performing parameter tuning with 500 randomly sampled data points.')
+    _, _, _, params, _ = svms[mdl_type](df.sample(n=500, random_state=2022).reset_index(drop=True), predictors,
+              to_predict_input, param_grid=param_grid, kernel=kernel, n_repeats=1, verbose=0)
+    param_grid = {par: _expspace([np.min(params[f'{par}_optimal']), np.max(params[f'{par}_optimal'])]) for par in param_grid}
+  logging.info(f'Training {mdl_type} model...')
+  df['predicted'], mdl, meta_data.stats, meta_data.params, meta_data.cv_folds = svms[mdl_type](df, predictors,
+              to_predict_input, param_grid=param_grid, kernel=kernel, verbose=verbose)
+  meta_data.cv_results = df[list(dict.fromkeys(['ID', 'Age', 'Sex', to_predict, 'predicted']))]
+  
   # Save model
   if save_path is not None:
-    if ~save_path.endswith('.pkl.gz'):
-      save_path = save_path + '.pkl.gz'
+    save_path = save_path.rstrip('.pkl.gz') + '.pkl.gz'
     with gzip.open(save_path, 'wb') as f:
       pickle.dump((mdl, vars(meta_data)), f)
       logging.info(f'Model saved to {save_path}')
@@ -102,7 +90,6 @@ def spare_train(df: Union[pd.DataFrame, str],
 
 def spare_test(df: Union[pd.DataFrame, str],
                mdl_path: Union[str, Tuple[dict, dict]]) -> pd.DataFrame:
-
   """Applies a trained SPARE model on a test dataset
 
   Args:
@@ -113,48 +100,41 @@ def spare_test(df: Union[pd.DataFrame, str],
   Returns:
     a pandas dataframe containing predicted SPARE scores.
   """
-
   df = _load_df(df)
 
   # Load trained SPARE model
-  if isinstance(mdl_path, str):
-    mdl, meta_data = load_model(mdl_path)
-  else:
-    mdl, meta_data = mdl_path
-    
+  mdl, meta_data = load_model(mdl_path) if isinstance(mdl_path, str) else mdl_path
   check_test(df, meta_data)
 
   # Convert categorical variables
-  for var in meta_data.get('categorical_var_map',{}).keys():
-    if not isinstance(meta_data['categorical_var_map'][var], dict):
+  for var, map_dict in meta_data.get('categorical_var_map',{}).items():
+    if not isinstance(map_dict, dict):
       continue
-    if np.all(df[var].isin(meta_data['categorical_var_map'][var].keys())):
-      df[var] = df[var].map(meta_data['categorical_var_map'][var])
+    if df[var].isin(map_dict.keys()).any():
+      df[var] = df[var].map(map_dict)
     else:
-      expected_var = list(meta_data['categorical_var_map'][var].keys())
+      expected_var = list(map_dict.keys())
       return logging.error(f'Column "{var}" expected {expected_var}, but received {list(df[var].unique())}')
 
   # Output model description
-  print('Model Info: training N =', meta_data['n'], end=' / ')
-  print('ages =', int(meta_data['age_range'][0]), '-', int(meta_data['age_range'][1]), end=' / ')
-  if meta_data['spare_type'] == 'classification':
-    print('expected AUC =', np.round(np.mean(meta_data['auc']), 3))
-  elif meta_data['spare_type'] == 'regression':
-    print('expected MAE =', np.round(np.mean(meta_data['mae']), 3))
-
+  print('Model Info: training N =', len(meta_data['cv_results'].index), end=' / ')
+  print('ages =', int(np.floor(np.min((meta_data['cv_results']['Age'])))),
+             '-', int(np.ceil(np.max((meta_data['cv_results']['Age'])))), end=' / ')
+  stats_metric = list(meta_data['stats'].keys())[0]
+  print(f'expected {stats_metric} =', '{:.3f}'.format(np.mean(meta_data['stats'][stats_metric])))
+  
   # Calculate SPARE scores
   n_ensemble = len(mdl['scaler'])
-  ss, ss_mean = np.zeros([len(df.index), n_ensemble]), np.zeros([len(df.index), ])
+  ss = np.zeros([len(df.index), n_ensemble])
   for i in range(n_ensemble):
     X = mdl['scaler'][i].transform(df[meta_data['predictors']])
     if meta_data['kernel'] == 'linear':
       ss[:, i] = np.sum(X * mdl['mdl'][i].coef_, axis=1) + mdl['mdl'][i].intercept_
     else:
       ss[:, i] = mdl['mdl'][i].decision_function(X)
-    if meta_data['spare_type'] == 'regression':
+    if meta_data['mdl_type'] == 'SVM Regression':
       ss[:, i] = (ss[:, i] - mdl['bias_correct']['int'][i]) / mdl['bias_correct']['slope'][i]
-    ss[df['ID'].isin(
-      meta_data['cv_results']['ID'][mdl['cv_folds'][i][0]]), i] = np.nan
+    ss[df['ID'].isin(meta_data['cv_results']['ID'].drop(meta_data['cv_folds'][i])), i] = np.nan
   ss_mean = np.nanmean(ss, axis=1)
   ss_mean[np.all(np.isnan(ss),axis=1)] = np.nan
 
@@ -164,7 +144,4 @@ def _expspace(span: list):
   return np.exp(np.linspace(span[0], span[1], num=int(span[1])-int(span[0])+1))
 
 def _load_df(df: Union[pd.DataFrame, str]) -> pd.DataFrame:
-  if isinstance(df, str):
-    return pd.read_csv(df, low_memory=False)
-  else:
-    return df.copy()
+  return pd.read_csv(df, low_memory=False) if isinstance(df, str) else df.copy()
