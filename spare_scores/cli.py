f3e090c84521746bb70f308ffd3891731ae50373
Spiros Maggioros <spirosastro@gmail.com>
2024-07-19 11:59:08 +0300

    Fixed possible bug with filename and package

--- spare_scores/cli.py
+++ spare_scores/cli.py
@@ -1,8 +1,7 @@
 import argparse
 
 import pkg_resources  # type: ignore
-
-from spare_scores import spare_test, spare_train
+from spare import spare_test, spare_train
 
 VERSION = pkg_resources.require("spare_scores")[0].version
 
